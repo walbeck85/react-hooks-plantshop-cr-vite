0999ef64e613df8dc7b51527745feb48cdc13df2
Will Albeck <will@albeck.me>
2025-09-10 18:58:17 -0400

    Fetch and store plant data on initial render via useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  // Fetch all plants from the server when the app first loads
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
+        } else {
+          console.error("Failed to fetch plants");
+        }
+      })
+      .then((plantsData) => setPlants(plantsData))
+      .catch((error) => console.error("Error fetching plants:", error));
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} />
     </div>
   );
 }
