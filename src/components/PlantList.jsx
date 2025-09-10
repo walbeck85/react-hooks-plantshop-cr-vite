76a79681d4ec4045a6d2a981629309ca30a8eb21
Will Albeck <will@albeck.me>
2025-09-10 19:07:18 -0400

    Fix: Ensure plants state is passed to PlantPage as props

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList({ plants }) {
+  if (!plants) {
+    return <p>Loading plants...</p>;
+  }
+
   return (
     <ul className="cards">
       {plants.map((plant) => (
